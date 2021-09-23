04058a814435767bd3f57a9f9cade9ed1837ebf2
augustocbx <augustocbx@gmail.com>
2021-09-23 17:00:12 -0300

    feat: Create ShorUrl on Controller

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,15 +1,21 @@
-class ShortUrlsController < ApplicationController
+class ShortUrlsController < ApplicationApiController
 
   # Since we're working on an API, we don't have authenticity tokens
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def index
   end
 
   def create
+    @url = ShortUrl.create(short_url_params)
+    render :show
   end
 
   def show
+  end
+
+  def short_url_params
+    params.permit(:full_url)
   end
 
 end
