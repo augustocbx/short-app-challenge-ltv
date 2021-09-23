1fde54c49dc99d79a8a34f95b11da2f9c456e916
augustocbx <augustocbx@gmail.com>
2021-09-23 17:22:48 -0300

    feat: validate required presence of full_url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,6 +2,7 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  validates :full_url, presence: true
   validate :validate_full_url
 
   def short_code
