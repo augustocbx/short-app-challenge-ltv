ba2e8f27245cf91de9bae69d4be4b451a1192641
Augusto Xavier <augusto.cesar@mpgo.mp.br>
2021-09-28 10:10:17 -0300

    Initial config for Webpack + Vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const vueConfig = require('./vue_config')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+environment.config.merge(vueConfig);
 module.exports = environment
