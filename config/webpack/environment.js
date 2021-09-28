094d4b5c3aabeac40e3c7f30b65287929f756577
Augusto Xavier <augusto.cesar@mpgo.mp.br>
2021-09-28 12:58:13 -0300

    Add configs to Vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
+const vueConfig = require('./vue_config')
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+environment.config.merge(vueConfig);
 module.exports = environment
